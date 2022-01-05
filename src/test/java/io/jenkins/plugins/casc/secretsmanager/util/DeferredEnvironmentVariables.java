31038fc96c85aff9346e99a9bae3accf912768f7
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2022-01-05 16:02:01 +0000

    Clean up DeferredEnvironmentVariables

--- src/test/java/io/jenkins/plugins/casc/secretsmanager/util/DeferredEnvironmentVariables.java
+++ src/test/java/io/jenkins/plugins/casc/secretsmanager/util/DeferredEnvironmentVariables.java
@@ -26,107 +26,104 @@ public class DeferredEnvironmentVariables implements TestRule {
      * Specify an env var whose value is immediately available.
      */
     public DeferredEnvironmentVariables set(String name, String value) {
-        this.buffer.put(name, () -> value);
+        buffer.put(name, () -> value);
         return this;
     }
 
-    private void writeVariableToEnvMap(String name, String value) {
-        this.set(getEditableMapOfVariables(), name, value);
-        this.set(getTheCaseInsensitiveEnvironment(), name, value);
-    }
-
-    private void set(Map<String, String> variables, String name, String value) {
-        if (variables != null) {
-            if (value == null) {
-                variables.remove(name);
-            } else {
-                variables.put(name, value);
-            }
-        }
-
-    }
-
-    private void copyVariablesFromBufferToEnvMap() {
-        for (Map.Entry<String, Supplier<String>> stringSupplierEntry : this.buffer.entrySet()) {
-            final String name = stringSupplierEntry.getKey();
-            final String value = stringSupplierEntry.getValue().get();
-            this.writeVariableToEnvMap(name, value);
-        }
-    }
-
     public Statement apply(Statement base, Description description) {
-        return new DeferredEnvironmentVariables.EnvironmentVariablesStatement(base);
+        return new DeferredEnvironmentVariables.EnvironmentVariablesStatement(base, buffer);
     }
 
-    private static Map<String, String> getEditableMapOfVariables() {
-        Class classOfMap = System.getenv().getClass();
+    private static class EnvironmentVariablesStatement extends Statement {
 
-        try {
-            return getFieldValue(classOfMap, System.getenv(), "m");
-        } catch (IllegalAccessException var2) {
-            throw new RuntimeException("System Rules cannot access the field 'm' of the map System.getenv().", var2);
-        } catch (NoSuchFieldException var3) {
-            throw new RuntimeException("System Rules expects System.getenv() to have a field 'm' but it has not.", var3);
-        }
-    }
+        private final Statement baseStatement;
+        private final Map<String, Supplier<String>> buffer;
+        private Map<String, String> originalVariables;
 
-    private static Map<String, String> getTheCaseInsensitiveEnvironment() {
-        try {
-            Class<?> processEnvironment = Class.forName("java.lang.ProcessEnvironment");
-            return getFieldValue(processEnvironment, (Object)null, "theCaseInsensitiveEnvironment");
-        } catch (ClassNotFoundException var1) {
-            throw new RuntimeException("System Rules expects the existence of the class java.lang.ProcessEnvironment but it does not exist.", var1);
-        } catch (IllegalAccessException var2) {
-            throw new RuntimeException("System Rules cannot access the static field 'theCaseInsensitiveEnvironment' of the class java.lang.ProcessEnvironment.", var2);
-        } catch (NoSuchFieldException var3) {
-            return null;
-        }
-    }
-
-    private static Map<String, String> getFieldValue(Class<?> klass, Object object, String name) throws NoSuchFieldException, IllegalAccessException {
-        Field field = klass.getDeclaredField(name);
-        field.setAccessible(true);
-        return (Map)field.get(object);
-    }
-
-    private class EnvironmentVariablesStatement extends Statement {
-        final Statement baseStatement;
-        Map<String, String> originalVariables;
-
-        EnvironmentVariablesStatement(Statement baseStatement) {
+        EnvironmentVariablesStatement(Statement baseStatement, Map<String, Supplier<String>> buffer) {
             this.baseStatement = baseStatement;
+            this.buffer = buffer;
         }
 
         public void evaluate() throws Throwable {
-            this.saveCurrentState();
+            saveCurrentState();
 
             try {
-                DeferredEnvironmentVariables.this.copyVariablesFromBufferToEnvMap();
-                this.baseStatement.evaluate();
+                copyVariablesFromBufferToEnvMap();
+                baseStatement.evaluate();
             } finally {
-                this.restoreOriginalVariables();
+                restoreOriginalVariables();
             }
-
         }
 
-        void saveCurrentState() {
-            this.originalVariables = new HashMap<>(System.getenv());
+        private void saveCurrentState() {
+            originalVariables = new HashMap<>(System.getenv());
         }
 
-        void restoreOriginalVariables() {
-            this.restoreVariables(DeferredEnvironmentVariables.getEditableMapOfVariables());
-            Map<String, String> theCaseInsensitiveEnvironment = DeferredEnvironmentVariables.getTheCaseInsensitiveEnvironment();
-            if (theCaseInsensitiveEnvironment != null) {
-                this.restoreVariables(theCaseInsensitiveEnvironment);
-            }
+        private void copyVariablesFromBufferToEnvMap() {
+            buffer.forEach((name, valueSupplier) -> {
+                final String value = valueSupplier.get();
+                writeVariableToEnvMap(name, value);
+            });
+        }
 
+        private void restoreOriginalVariables() {
+            restoreVariables(getEditableMapOfVariables());
+            restoreVariables(getTheCaseInsensitiveEnvironment());
         }
 
         void restoreVariables(Map<String, String> variables) {
-            variables.clear();
-            variables.putAll(this.originalVariables);
+            if (variables != null) {
+                variables.clear();
+                variables.putAll(originalVariables);
+            }
+        }
+
+        private static void writeVariableToEnvMap(String name, String value) {
+            set(getEditableMapOfVariables(), name, value);
+            set(getTheCaseInsensitiveEnvironment(), name, value);
+        }
+
+        private static Map<String, String> getEditableMapOfVariables() {
+            final Class<?> classOfMap = System.getenv().getClass();
+
+            try {
+                return (Map<String, String>) getFieldValue(classOfMap, System.getenv(), "m");
+            } catch (IllegalAccessException var2) {
+                throw new RuntimeException("Rule cannot access the field 'm' of the map System.getenv().", var2);
+            } catch (NoSuchFieldException var3) {
+                throw new RuntimeException("Rule expects System.getenv() to have a field 'm' but it has not.", var3);
+            }
+        }
+
+        private static Map<String, String> getTheCaseInsensitiveEnvironment() {
+            try {
+                final Class<?> processEnvironment = Class.forName("java.lang.ProcessEnvironment");
+
+                return (Map<String, String>) getFieldValue(processEnvironment, null, "theCaseInsensitiveEnvironment");
+            } catch (ClassNotFoundException var1) {
+                throw new RuntimeException("Rule expects the existence of the class java.lang.ProcessEnvironment but it does not exist.", var1);
+            } catch (IllegalAccessException var2) {
+                throw new RuntimeException("Rule cannot access the static field 'theCaseInsensitiveEnvironment' of the class java.lang.ProcessEnvironment.", var2);
+            } catch (NoSuchFieldException var3) {
+                return null;
+            }
+        }
+
+        private static Object getFieldValue(Class<?> klass, Object object, String name) throws NoSuchFieldException, IllegalAccessException {
+            final Field field = klass.getDeclaredField(name);
+            field.setAccessible(true);
+            return field.get(object);
+        }
+
+        private static void set(Map<String, String> variables, String name, String value) {
+            if (variables != null) {
+                if (value == null) {
+                    variables.remove(name);
+                } else {
+                    variables.put(name, value);
+                }
+            }
         }
     }
-
-
 }
